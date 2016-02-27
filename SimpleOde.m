e28f982e830f1979266a352dcef371761bf4c638
Sarah Sahibzada <srsahibzada@gmail.com>
2016-02-27 13:12:33 -0600

    Update SimpleOde.m

--- SimpleOde.m
+++ SimpleOde.m
@@ -7,11 +7,9 @@ declination = 23.5;
 eccentricity = 0.016;
 radians = 2*pi/360;
 frequency = 2*pi/365.25;
-mean_distance = 92955807;
 solar_intensity = 1370/pi;
 sigma = 5.67*10^-8;
 radius_of_earth = 6371*1000;
-sol = 1.361;
 density = 1; % density of water is 1000 kgm^-3
 sphc = 4.186;
 hmix = 1000;
@@ -23,7 +21,7 @@ hmix = 1000;
 solar_angle = cos((lat*(pi/180) - (declination*(pi/180)) * cos(frequency*(t-170))));
 distance_from_sun =  (1-eccentricity * cos(frequency * t - 10))^2;
 Qin = solar_intensity*(solar_angle / distance_from_sun)
-Qout = sigma*temp^4 * 4*pi*radius_of_earth;
+Qout = sigma*temp^4 * 4*pi*radius_of_earth^2;
 %
 
 dy = (Qin-Qout)*(1/(density * sphc * hmix));
