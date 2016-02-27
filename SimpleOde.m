ab22de97384bcb78bc70d6f69d31fc0878afcf64
StephenCapps <durdleman@tamu.edu>
2016-02-27 13:50:20 -0600

    Update SimpleOde.m

--- SimpleOde.m
+++ SimpleOde.m
@@ -1,6 +1,6 @@
 function dy = SimpleOde(t,temp)
 %
-lat = 30.0;   % Set the latitude here
+lat = 0;   % Set the latitude here
 %
 
 declination = 23.5;
@@ -9,7 +9,7 @@ radians = 2*pi/360;
 frequency = 2*pi/365.25;
 solar_intensity = 1370/pi;
 sigma = 5.67*10^-8;
-radius_of_earth = 6371*1000;
+radius_of_earth = (6371*1000).^2;
 density = 1; % density of water is 1000 kgm^-3
 sphc = 4.186;
 hmix = 1000;
@@ -20,8 +20,8 @@ hmix = 1000;
 %
 solar_angle = cos((lat*(pi/180) - (declination*(pi/180)) * cos(frequency*(t-170))));
 distance_from_sun =  (1-eccentricity * cos(frequency * t - 10))^2;
-Qin = solar_intensity*(solar_angle / distance_from_sun)
-Qout = sigma*temp^4 * 4*pi*radius_of_earth^2;
+Qin = solar_intensity*(solar_angle / distance_from_sun);
+Qout = sigma*temp.^4 * 4*pi*radius_of_earth;
 %
 
 dy = (Qin-Qout)*(1/(density * sphc * hmix));
@@ -29,27 +29,6 @@ dy = (Qin-Qout)*(1/(density * sphc * hmix));
 
 
 % Solve.m
-clear all;
-
-close all;
-
-tspan = [0:1:10*365];
-
-%
-
-t0 =        30            ; %initial condition here
-
-%
-
-[t,temp] = ode45('SimpleOde',tspan,t0)
-
-%
-
-plot(t,temp);
-title('insert title here');
-xlabel('time');
-ylabel('temp');
-
 
 
 end
